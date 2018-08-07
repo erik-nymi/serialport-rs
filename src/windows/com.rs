7f245ed528f1a142cba724ecb6b38aeaaa19753e
Nick Coish <nick.coish@gmail.com>
2018-08-06 22:59:33 -0400

    temp

--- src/windows/com.rs
+++ src/windows/com.rs
@@ -96,6 +96,9 @@ impl COMPort {
             let mut com = COMPort::open_from_raw_handle(handle as RawHandle);
             com.port_name = port.as_ref().to_str().map(|s| s.to_string());
             com.set_all(settings)?;
+
+            let mask: u32 = 1;
+            unsafe { SetCommMask(handle, mask) };
             Ok(com)
         } else {
             Err(super::error::last_os_error())
